451cab11f74b454db29d5695cbb5bb1fce8888b3
Alexandre-Rio <alexandrerio5691@gmail.com>
2020-12-30 22:35:23 +0100

    Minor updates on filtering.py

--- filtering.py
+++ filtering.py
@@ -15,7 +15,7 @@ import pdb
 
 os.chdir(os.getcwd())  # Change directory to current working directory
 time_range = 82
-use_validation = True
+use_validation = False
 
 # Load data
 local_case_data_onset = pd.read_csv('data/local_case_data_onset.csv', encoding='cp1252')
@@ -49,6 +49,7 @@ data = format_data(data_dict)
 
 # Define model
 model = TransmissionModelExtended(**theta)
+model.use_validation = use_validation
 
 # Define Feyman-Kac model
 fk_model = ssm.Bootstrap(ssm=model, data=data)
@@ -58,7 +59,7 @@ if __name__ == '__main__':
     # Define SMC algorithm parameters
     n_particles = 2000  # Number of particles
     n_runs = 200  # Number of runs
-    resampling_mtd = 'multinomial'
+    resampling_mtd = 'ssp'
 
     # Run algorithm n_runs times / for one run, just do "pf.run()"
     samples = []
@@ -77,21 +78,35 @@ if __name__ == '__main__':
         samples = samples[0]
     samples = np.array(samples)
 
-
-    # Plot estimated R0
-    x = np.arange(time_range)
+    # Compute quantities of interest
+    period_interest = np.arange(22, 82)  # 2019-12-14 to end date
     r0 = samples['beta'] / theta['gamma']
     r0_mean = r0.mean(axis=0)
     r0_std = r0.std(axis=0)
     r0_ci = 1.96 * r0_std / np.sqrt(n_runs)
+    r0_quantiles = np.quantile(r0, q=[0.025, 0.25, 0.5, 0.75, 0.975], axis=0)
+
+    # Plot estimated R0 (mean and std over all runs)
+    x = period_interest
     plt.figure()
     plt.title('R0 - Estimated by SMC ({} runs, {} particles)'.format(n_runs, n_particles))
-    plt.plot(x, r0_mean, color='royalblue')
-    plt.fill_between(x, r0_mean - r0_ci, r0_mean + r0_ci, color='lightblue')
+    plt.plot(x, r0_mean[x], color='royalblue')
+    plt.fill_between(x, r0_mean[x] - r0_ci[x], r0_mean[x] + r0_ci[x], color='lightblue')
+    plt.ylim(0, 12)
     plt.xlabel('Time steps')
     plt.ylabel('Estimated R0')
     plt.show()
 
+    # Plot estimated R0 (quantiles 0.025, 0.25, 0.5, 0.75, 0.975)
+    plt.figure()
+    plt.title('R0 - Estimated by SMC ({} runs, {} particles)'.format(n_runs, n_particles))
+    plt.fill_between(x, r0_quantiles[0, period_interest], r0_quantiles[4, period_interest], color='lightblue')
+    plt.fill_between(x, r0_quantiles[1, period_interest], r0_quantiles[3, period_interest], color='tab:blue')
+    plt.plot(x, r0_quantiles[2, period_interest], color='black')
+    plt.ylim(0, 12)
+    plt.xlabel('Time steps')
+    plt.ylabel('Estimated R0')
+    plt.show()
 
 end=True
 
